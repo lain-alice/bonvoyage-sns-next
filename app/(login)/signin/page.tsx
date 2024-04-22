b3ebd633e4cfd8b446c426821adc970d7bd589d4
lain-alice <freya6829@gmail.com>
2024-04-22 15:56:05 +0900

    컴포넌트명 수정, ui 폴더 삭제

--- app/(login)/signin/page.tsx
+++ app/(login)/signin/page.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
 
-const App = () => {
+const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -71,4 +71,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default SignIn;
